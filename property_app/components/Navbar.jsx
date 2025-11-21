932f7a39b86980c6d40690d1ac352233bbd476c8
jymax4rl <116975025+jymax4rl@users.noreply.github.com>
2025-11-21 23:09:44 +0100

    formatted the Navbar

--- property_app/components/Navbar.jsx
+++ property_app/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import logIcon from "@/assets/images/person.png";
+import logo from "@/assets/images/blackAnkhLogo.png";
+import Hamburger from "@/components/hamburger";
 
 const Navbar = () => {
-  return <div className="border-4 border-green-500 h-[8vh] m-0">Navbar</div>;
+  return (
+    <nav className="grid grid-cols-3 grid-gap-4 h-[8vh] border-4">
+      <div className=" w-20 lg:w-40 flex items-center justify-center align-center">
+        <Image
+          className="lg:h-13 h-9 cursor-pointer w-auto rounded-full"
+          alt="logo"
+          src={logo}
+        />
+      </div>
+
+      <div className=" flex items-center justify-center align-center"></div>
+
+      <div className=" flex  lg:w-20 m-0  lg:ml-80 space-x-5 items-center justify-center align-center">
+        <div className="flex   items-center justify-center space-x-8 align-center lg:absolute ">
+          <Image
+            className="cursor-pointer h-5 w-auto"
+            alt="login Icon"
+            src={logIcon}
+          />
+          <Hamburger />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
