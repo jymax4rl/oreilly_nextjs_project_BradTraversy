20252530d7852421fd00f216c66801f975fd7ebd
jymax4rl <116975025+jymax4rl@users.noreply.github.com>
2025-11-22 02:12:38 +0100

    styled the navbar links

---
+++ property_app/components/NavButton.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const NavButton = ({ text }) => {
+  return (
+    <StyledWrapper>
+      <button className="cta">
+        <span className="hover-underline-animation"> {text} </span>
+      </button>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  .cta {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+
+  .cta span {
+    padding-bottom: 7px;
+    letter-spacing: 2px;
+    font-size: 12px;
+    text-transform: uppercase;
+  }
+
+  .hover-underline-animation {
+    position: relative;
+    color: black;
+    padding-bottom: 20px;
+  }
+
+  .hover-underline-animation:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 1px;
+    bottom: 0;
+    left: 0;
+    background-color: #000000;
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  .cta:hover .hover-underline-animation:after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+`;
+
+export default NavButton;
--- property_app/components/Navbar.jsx
+++ property_app/components/Navbar.jsx
@@ -4,12 +4,13 @@ import logIcon from "@/assets/images/person.png";
 import logo from "@/assets/images/blackAnkhLogo.png";
 import Hamburger from "@/components/hamburger";
 import Link from "next/link";
+import NavButton from "./NavButton";
 
 const Navbar = () => {
   return (
     // The nav is set to 3 col in md screens & 2 cols anything less
     <nav className="m-0 grid bg-white grid-cols-2 lg:grid-cols-[20%_60%_20%] h-[8vh]">
-      <div className=" flex items-center ml-10 lg:ml-22 justify-start  lg:justify-start align-center">
+      <div className=" flex items-center ml-10 lg:ml-22 justify-start  align-center">
         <Link href={"/"}>
           <Image
             className="lg:h-13 h-12 cursor-pointer w-auto rounded-full"
@@ -19,7 +20,17 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="hidden lg:block bg-green-900 flex items-center justify-center align-center"></div>
+      <div className="hidden lg:flex space-x-16 items-center justify-center align-center">
+        <Link href={"/"}>
+          <NavButton text={"Home"} />
+        </Link>
+        <Link href={"/properties"}>
+          <NavButton text={"Properties"} />
+        </Link>
+        <Link href={"/properties/Properties"}>
+          <NavButton text={"Add Property"} />
+        </Link>
+      </div>
 
       <div className=" flex p-0  mr-6 lg:m-0   items-center justify-end lg:justify-center   ">
         <div className="flex start items-center justify-center space-x-4 md:space-x-8 align-center lg:absolute ">
