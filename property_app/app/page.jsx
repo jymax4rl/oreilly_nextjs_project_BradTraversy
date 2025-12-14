f27ab97ed65594b03e6678e1a0991b8cc367535f
jymax4rl <116975025+jymax4rl@users.noreply.github.com>
2025-12-14 02:00:08 +0100

    fetching a limited amount of propertie for the homePage
    fetching a limited amount of propertie for the homePage with!
            <PropertiesPage maxProperties={10} />
    
    logic:
    /limit of 10 properties
      const limitedProperties = serializedProperties.slice(0, maxProperties);
    
      return (
        <div className="">
          {/* Pass server-fetched properties to client component */}
          <HomeProperties initialProperties={limitedProperties} />
        </div>
      );

--- property_app/app/page.jsx
+++ property_app/app/page.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import Footer from "@/components/Footer";
-import { MongoClient } from "mongodb";
-import connectToDatabase from "@/config/database";
+import HomeProperties from "@/components/HomeProperties";
+import PropertiesPage from "./properties/page";
 const HomePage = () => {
   return (
     <div className="m-0 p-0  ">
       <div className="w-full ">
         <Hero className="h-[55vh] border-4 border-zinc-800" />
+        <PropertiesPage maxProperties={10} />
         <InfoBoxes />
       </div>
     </div>
--- property_app/app/properties/page.jsx
+++ property_app/app/properties/page.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import HomeProperties from "@/components/HomeProperties";
-import connectToDatabase from "@/config/database";
 import Property from "@/models/Property";
 
-const PropertiesPage = async () => {
-  // Fetch properties on the server
-  await connectToDatabase();
-  const properties = await Property.find({}).lean();
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/properties`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async ({ maxProperties }) => {
+  const properties = await fetchProperties();
+  //sort propeties by name
+  properties.sort((a, b) => a.name.localeCompare(b.name));
+
+  //sort properties by date
+  // properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   // Convert MongoDB ObjectIds to strings for serialization
   const serializedProperties = properties.map((property) => ({
+    // spread all properties
     ...property,
+    // Convert ObjectId to string
     _id: property._id.toString(),
   }));
+
+  //limit of 10 properties
+  const limitedProperties = serializedProperties.slice(0, maxProperties);
 
   return (
     <div className="">
       {/* Pass server-fetched properties to client component */}
-      <HomeProperties initialProperties={serializedProperties} />
+      <HomeProperties initialProperties={limitedProperties} />
     </div>
   );
 };
